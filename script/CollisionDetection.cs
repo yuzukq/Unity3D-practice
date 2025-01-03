63044e0a093ef4000ab4e8de9058fe8068807240
VR-Yuzu <109357821+yuzukq@users.noreply.github.com>
2025-01-03 17:01:40 +0900

    Add files via upload

--- script/CollisionDetection.cs
+++ script/CollisionDetection.cs
@@ -6,10 +6,11 @@ public class CollisionDetection : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Acquisiton")
+        if (other.tag == "Acquisition")
         {
+            GameManager.instance.AddScore();
+            //Debug.Log("衝突を検知");
             Destroy(other.gameObject);
         }
-
     }
 }
--- script/GameManager.cs
+++ script/GameManager.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance; //GameManager型の変数instance
+    public static GameManager instance;
     //staticをつけるとインスタンス単位で生成されるのではなく，アプリ単位で一つだけ生成される．
-    //->インスタンスの参照をせずに直接GameManagerにの関数，変数にアクセスできるようになる．
+    //->インスタンスの参照をせずに直接GameControllerにの関数，変数にアクセスできるようになる．
+
+    private int totalScore = 0;  // 衝突回数を記録する変数
 
     private void Awake()//Awake()はゲーム開始時にStart()より先に呼ばれる．
     {
-        instance = this; // このGameManagerをinstanceに格納
+        instance = this;
     }
 
-    public void TestStatic()
+    // スコアを増やす関数
+    public void AddScore()
     {
-        Debug.Log("staticのテスト中");
+        totalScore++;
+        Debug.Log($"現在のスコア: {totalScore}");
+    }
+
+    // スコアを取得する関数
+    public int GetScore()
+    {
+        return totalScore;
     }
 
 }
